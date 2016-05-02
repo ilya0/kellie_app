557e2b71437587682779f3a04e54b100438bebdc
Gagandeep Singh <singh.enso@gmail.com>
2016-05-02 15:31:01 -0700

    before using Angular

--- app.js
+++ app.js
@@ -8,8 +8,32 @@ var routes        = require('./routes/index');
 var instructors   = require('./routes/instructor');
 var session       = require('express-session');
 var db            = require('./config/db');
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
 var app           = express();
 
+
+//middleware
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://www.example.com/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }));
+
+app.get('/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
 
 // Routes
 var instructorRoute = require('./routes/instructor.js');
