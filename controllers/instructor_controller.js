3b921c8daf686a18a2fb4fcdd284b1459c770876
Gagandeep Singh <singh.enso@gmail.com>
2016-05-02 12:01:32 -0700

    add express and routes

---
+++ controllers/instructor_controller.js
@@ -0,0 +1,18 @@
+var instructorController = {};
+
+instructorController.index = function(req, res) {};
+
+instructorController.new = function(req, res) {};
+
+instructorController.create = function(req, res) {};
+
+instructorController.update = function(req, res) {};
+
+instructorController.show = function(req, res) {};
+
+instructorController.edit = function(req, res) {};
+
+instructorController.destroy = function(req, res) {};
+
+module.exports = instructorController;
+
